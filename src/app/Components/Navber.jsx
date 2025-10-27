0758434d6add29f3793becc563ae72d291ab4f6d
abunaim34 <farhanadnanfarabi@gmail.com>
2025-10-27 19:36:26 +0600

    navber searchbar

--- src/app/Components/Navber.jsx
+++ src/app/Components/Navber.jsx
@@ -23,9 +23,27 @@ const Navber = () => {
                         <li><a>Item 3</a></li>
                     </ul>
                 </div>
-                <Link href={'/'} className="">
-                    <Image src='/LinkerLogo.png' alt="LINKER" width={150} height={30} className="mr-2" />
-                </Link> 
+                {/* Logo & Searchbar */}
+                <div className="flex gap-4 items-center">
+                    <Link href={'/'} className="">
+                        <Image src='/LinkerLogo.png' alt="LINKER" width={150} height={30} className="mr-2" />
+                    </Link>
+                    <label className="input rounded-full">
+                        <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <g
+                                strokeLinejoin="round"
+                                strokeLinecap="round"
+                                strokeWidth="2.5"
+                                fill="none"
+                                stroke="currentColor"
+                            >
+                                <circle cx="11" cy="11" r="8"></circle>
+                                <path d="m21 21-4.3-4.3"></path>
+                            </g>
+                        </svg>
+                        <input type="search" required placeholder="Search" />
+                    </label>
+                </div>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
