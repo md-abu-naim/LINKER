38319c905e5741444ccbb3edcb8ad7b851e308ba
abunaim34 <farhanadnanfarabi@gmail.com>
2025-10-28 06:24:54 +0600

    navber > leftside responsive complete

--- src/app/Components/Navber/Navber.jsx
+++ src/app/Components/Navber/Navber.jsx
@@ -22,26 +22,9 @@ const Navber = () => {
            <nav className="w-full bg-gray-900 border-b border-gray-800 shadow-md sticky z-50">
                 <div className="px-4 py-3 flex justify-between items-center">
                     {/* Logo + Serchbar */}
-                    <div className="flex items-center gap-2">
+                    <div className="flex items-center gap-2 relative">
                         <Image src='/Logo.png' alt="Linker" className=" object-contain" width={40} height={30} />
                         <span className="text-xl font-bold text-cyan-400">LINKER</span>
-
-                        <div className="hidden lg:flex items-center bg-gray-800 rounded-full px-3 py-2 ml-3">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="h-5 w-5 text-gray-400"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                            >
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-4.35-4.35M11 18a7 7 0 100-14 7 7 0 000 14z" />
-                            </svg>
-                            <input
-                                type="search"
-                                placeholder="Search Linker"
-                                className="bg-transparent outline-none text-sm px-2 w-40 focus:w-44 transition-all duration-300"
-                            />
-                        </div>
                         <ToggleSerchbar />
                     </div>
                 </div>
--- src/app/Components/Navber/ToggleSerchbar.jsx
+++ src/app/Components/Navber/ToggleSerchbar.jsx
@@ -6,11 +6,7 @@ const ToggleSerchbar = () => {
     const [active, setActive] = useState(false);
     return (
         <div className="">
-            <div onClick={() => setActive(!active)} className="lg:hidden bg-gray-800 rounded-full text-gray-400 p-2 w-8 h-8">
-                <span><FaSearch></FaSearch></span>
-            </div>
-            <div className="">
-                <div className={`${active} flex items-center bg-gray-800 rounded-full px-3 py-2 ml-3`}>
+            <div className="hidden lg:flex items-center bg-gray-800 rounded-full px-3 py-2 ml-3">
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="h-5 w-5 text-gray-400"
@@ -26,7 +22,31 @@ const ToggleSerchbar = () => {
                     className="bg-transparent outline-none text-sm px-2 w-40 focus:w-44 transition-all duration-300"
                 />
             </div>
+
+            {/* Small Screen Searchbar */}
+            <div onClick={() => setActive(!active)} className={`${active ? 'hidden' : 'block'} lg:hidden bg-gray-800 rounded-full text-gray-400 p-2 w-8 h-8`}>
+                <span><FaSearch></FaSearch></span>
             </div>
+            <div className={`${active ? 'block absolute top-0 left-0' : 'hidden'} lg:hidden `}>
+                <div className={` flex items-center bg-gray-800 rounded-full px-3 py-2 ml-3`}>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-5 w-5 text-gray-400"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                    >
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-4.35-4.35M11 18a7 7 0 100-14 7 7 0 000 14z" />
+                    </svg>
+                    <input
+                        type="search"
+                        placeholder="Search Linker"
+                        className="bg-transparent outline-none text-sm px-2 w-fit focus:w-44 transition-all duration-300"
+                    />
+                </div>
+                <div onClick={() => setActive(false)} className="fixed inset-0 bg-transparent z-40"></div>
+            </div>
+
         </div>
     );
 };
