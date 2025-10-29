7f02d4e5bb5cf186332c774d9e4e93619f28c538
abunaim34 <farhanadnanfarabi@gmail.com>
2025-10-29 16:48:09 +0600

    navber avater dropdwon compelte

--- src/app/Components/Navber/Navber.jsx
+++ src/app/Components/Navber/Navber.jsx
@@ -35,25 +35,19 @@ const Navber = () => {
 
                     {/* Right: Message, Notification, Avater Dropdwon */}
                     <div className="flex items-center justify-between gap-2 sm:gap-5">
-                        <Link href='/notifications' title="Notifications" className="relative bg-gray-800 p-1 px-1.5 rounded-full">
-                            <button className="text-xl md:text-2xl hover:text-cyan-400 transition-colors"><FaBell /></button>
-                            <span className="absolute -top-1.5 -right-1.5 bg-red-500 text-white font-semibold rounded-full w-5 h-4 text-[11px] flex items-center justify-center">9+</span>
-                        </Link>
-                        <Link href='/message' title="Message" className="relative bg-gray-800 p-1 px-1.5 rounded-full">
-                            <button className="text-xl md:text-2xl hover:text-cyan-400 transition-colors"><LuMessageCircleHeart /></button>
-                            <span className="absolute -top-1.5 -right-1.5 bg-red-500 text-white font-semibold rounded-full w-5 h-4 text-[11px] flex items-center justify-center">9+</span>
-                        </Link>
-                        {/* Dropdwon */}
-                        <div title="Account" className="dropdown">
-                            <button tabIndex={0} className="w-8 h-8 sm:w-9 sm:h-9 rounded-full overflow-hidden border-2 border-cyan-400">
-                                <Image src="" alt="User" width={24} height={20} className="w-24 h-6" />
+
+                        {/* Notification */}
+                        <div className="dropdown">
+                            <button tabIndex={0} title="Notifications" className="relative bg-gray-800 p-1 px-1.5 rounded-full">
+                                <button className="text-xl md:text-2xl hover:text-cyan-400 transition-colors"><FaBell /></button>
+                                <span className="absolute -top-1.5 -right-1.5 bg-red-500 text-white font-semibold rounded-full w-5 h-4 text-[11px] flex items-center justify-center">9+</span>
                             </button>
+
                             <div tabIndex="-1" className="dropdown-content z-50 mt-2 bg-gray-900 rounded-md shadow-lg w-2xs py-2 border right-0 border-cyan-700">
-                                <Link href="/profile" className="flex items-center gap-2 p-3 mx-3 mb-3 border border-gray-700 rounded-lg shadow-md hover:shadow-lg transition-all duration-200">
+                                <Link href="/profile" className="flex items-center gap-2 p-3 mx-3 mb-3">
                                     <Image src="" alt="User" width={24} height={20} className="inline-block w-9 h-9 border border-cyan-400 rounded-full " />
                                     <div className="flex flex-col">
                                         <span className="font-semibold text-sm">Mohammad Abu Naim</span>
-                                        <span className="text-gray-400 text-xs">View Profile</span>
                                     </div>
                                 </Link>
                                 <Link href="/settings" className="flex items-center gap-2 px-4 py-2 hover:bg-gray-700">
@@ -72,6 +66,42 @@ const Navber = () => {
                                     <span className="text-xl bg-gray-800 p-2 rounded-full"><MdLogout /></span>
                                     <span className="text-xl">Log Out</span>
                                 </Link>
+                            </div>
+                        </div>
+                        <Link href='/message' title="Message" className="relative bg-gray-800 p-1 px-1.5 rounded-full">
+                            <button className="text-xl md:text-2xl hover:text-cyan-400 transition-colors"><LuMessageCircleHeart /></button>
+                            <span className="absolute -top-1.5 -right-1.5 bg-red-500 text-white font-semibold rounded-full w-5 h-4 text-[11px] flex items-center justify-center">9+</span>
+                        </Link>
+
+                        {/* Avater Dropdwon */}
+                        <div className="dropdown">
+                            <button title="Account" tabIndex={0} className="w-8 h-8 sm:w-9 sm:h-9 rounded-full overflow-hidden border-2 border-cyan-400">
+                                <Image src="" alt="User" width={24} height={20} className="w-24 h-6" />
+                            </button>
+                            <div tabIndex="-1" className="dropdown-content z-50 mt-2 bg-gray-900 rounded-md shadow-lg w-2xs py-2 border right-0 border-cyan-700">
+                                <Link href="/profile" className="flex items-center gap-2 p-3 mx-3 mb-3 border border-gray-700 rounded-lg shadow-md hover:shadow-lg transition-all duration-200">
+                                    <Image src="" alt="User" width={24} height={20} className="inline-block w-9 h-9 border border-cyan-400 rounded-full " />
+                                    <div className="flex flex-col">
+                                        <span className="font-semibold text-sm">Mohammad Abu Naim</span>
+                                        <span className="text-gray-400 text-xs">View Profile</span>
+                                    </div>
+                                </Link>
+                                <Link href="/settings" className="flex items-center gap-2 px-4 py-2 hover:bg-gray-700">
+                                    <span className="text-xl bg-gray-800 p-2 rounded-full"><IoSettingsOutline /></span>
+                                    <span className="text-xl">Settings & Privacy</span>
+                                </Link>
+                                <Link href="/help&support" className="flex items-center gap-2 px-4 py-2 hover:bg-gray-700">
+                                    <span className="text-xl bg-gray-800 p-2 rounded-full"><MdHelpOutline /></span>
+                                    <span className="text-xl">Help & Support</span>
+                                </Link>
+                                <Link href="/feedback" className="flex items-center gap-2 px-4 py-2 hover:bg-gray-700">
+                                    <span className="text-xl bg-gray-800 p-2 rounded-full"><MdFeedback /></span>
+                                    <span className="text-xl">Give Feedback</span>
+                                </Link>
+                                <button className="flex items-center gap-2 px-4 py-2 hover:bg-gray-700">
+                                    <span className="text-xl bg-gray-800 p-2 rounded-full"><MdLogout /></span>
+                                    <span className="text-xl">Log Out</span>
+                                </button>
                             </div>
 
                         </div>
--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      hel
+      <h1 className='text-3xl text-center text-cyan-400 font-semibold pt-7'>Welcome to the LINKER</h1>
     </div>
   );
 };
