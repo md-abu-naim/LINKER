7e7c85b6d5eb19008497c6b4af873230ffe1223b
abunaim34 <farhanadnanfarabi@gmail.com>
2025-10-29 16:32:58 +0600

    navber dropdwon design

--- src/app/Components/Navber/MobileNavber.jsx
+++ src/app/Components/Navber/MobileNavber.jsx
@@ -20,7 +20,7 @@ const MobileNavber = () => {
                 <ul className="flex justify-around items-center">
                     {
                         navLinks?.map(link => (
-                            <li key={link.href} className={`${pathname === link.href && 'border-t-2 border-cyan-600'} py-2 px-3 hover:text-cyan-400 transition-colors`}>
+                            <li key={link.href} title={link.title} className={`${pathname === link.href && 'border-t-2 border-cyan-600'} py-2 px-3 hover:text-cyan-400 transition-colors`}>
                                 <Link href={link.href} className="flex flex-col items-center">
                                     {link.icon}
                                     <span>{link.title}</span>
--- src/app/Components/Navber/NavLinks.jsx
+++ src/app/Components/Navber/NavLinks.jsx
@@ -9,15 +9,15 @@ const NavLinks = () => {
 
     const navLinks = [
         { href: '/', icon:<FaHome className="text-2xl md:text-3xl" />, title: 'Home' },
-        { href: '/friends', icon:<FaUserFriends className="text-2xl md:text-3xl" />, title: 'Home' },
-        { href: '/explore', icon:<MdOutlineExplore className="text-2xl md:text-3xl" />, title: 'Home' },
+        { href: '/friends', icon:<FaUserFriends className="text-2xl md:text-3xl" />, title: 'Friends' },
+        { href: '/explore', icon:<MdOutlineExplore className="text-2xl md:text-3xl" />, title: 'Explore' },
     ]
     
     return (
         <ul className="hidden md:flex just items-center font-medium gap-6 lg:mr-24">
             {
                 navLinks?.map((link) => (
-                    <li key={link.href} className={`hover:text-cyan-400 ${pathname === link.href && 'text-cyan-500 border-cyan-600 border-b-2'} py-2 px-6 transition-colors`}>
+                    <li key={link.href} title={link.title} className={`hover:text-cyan-400 ${pathname === link.href && 'text-cyan-500 border-cyan-600 border-b-2'} py-2 px-6 transition-colors`}>
                         <Link href={link.href} >
                             {link.icon}
                         </Link>
