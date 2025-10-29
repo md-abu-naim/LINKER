61f1cf46b522c7bbef380617b741be7a452b018b
abunaim34 <farhanadnanfarabi@gmail.com>
2025-10-29 10:27:06 +0600

    navber responsive complete

--- src/app/Components/Navber/Navber.jsx
+++ src/app/Components/Navber/Navber.jsx
@@ -9,45 +9,45 @@ import ToggleSerchbar from "./ToggleSerchbar";
 const Navber = () => {
 
     const navLinks = <>
-        <li className="hover:text-cyan-400 transition cursor-pointer"><Link href='/' ><FaHome className="text-3xl" /></Link></li>
-        <li className="hover:text-cyan-400 transition cursor-pointer"><Link href='/explore'><MdOutlineOndemandVideo className="text-3xl" /></Link></li>
-        <li className="hover:text-cyan-400 transition cursor-pointer"><Link href='/friends'><FaUserFriends className="text-3xl" /></Link></li>
+        <li className="hover:text-cyan-400 transition cursor-pointer"><Link href='/' ><FaHome className="text-xl md:text-3xl" /></Link></li>
+        <li className="hover:text-cyan-400 transition cursor-pointer"><Link href='/explore'><MdOutlineOndemandVideo className="text-xl md:text-3xl" /></Link></li>
+        <li className="hover:text-cyan-400 transition cursor-pointer"><Link href='/friends'><FaUserFriends className="text-xl md:text-3xl" /></Link></li>
     </>
     return (
         <div>
             <nav className="w-full bg-gray-900 border-b border-gray-800 shadow-md sticky z-50">
-                <div className="px-4 py-3 flex justify-between items-center">
+                <div className="flex justify-between items-center px-3 sm:px-4 py-2">
                     {/*Left: Logo + Serchbar */}
                     <div className="flex items-center gap-2 relative">
-                        <Image src='/Logo.png' alt="Linker" width={40} height={30} />
+                        <Image src='/Logo.png' alt="Linker" width={38} height={28} className="w-9 h-auto" />
                         <span className="hidden lg:block text-xl font-bold text-cyan-400">LINKER</span>
                         <ToggleSerchbar />
                     </div>
 
                     {/*Center: Navlinks */}
-                    <ul className="flex items-center justify-center font-medium space-x-5 lg:mr-24">
+                    <ul className="flex items-center font-medium gap-4 sm:gap-6 lg:mr-24">
                         {navLinks}
                     </ul>
 
                     {/* Right: Message, Notification, Avater Dropdwon */}
-                    <div className="flex items-center justify-between gap-5">
+                    <div className="flex items-center justify-between gap-2 sm:gap-5">
                         <Link href='/notifications' title="Cart Items" className="relative">
-                            <button className="text-3xl hover:text-cyan-400 transition-colors"><FaBell /></button>
-                            <span className="absolute -top-2 -right-2 bg-cyan-500 text-white text-xs font-semibold rounded-full w-5 h-5 flex items-center justify-center">9+</span>
+                            <button className="text-xl md:text-3xl hover:text-cyan-400 transition-colors"><FaBell /></button>
+                            <span className="absolute -top-1.5 -right-1.5 bg-cyan-500 text-white font-semibold rounded-full w-4 h-4 text-[10px] flex items-center justify-center">9+</span>
                         </Link>
                         <Link href='/message' title="Cart Items" className="relative z-10">
-                            <button className="text-3xl hover:text-cyan-400 transition-colors"><LuMessageCircleHeart /></button>
+                            <button className="text-xl md:text-3xl hover:text-cyan-400 transition-colors"><LuMessageCircleHeart /></button>
                             <span className="absolute -top-2 -right-2 bg-cyan-500 text-white text-xs font-semibold rounded-full w-5 h-5 flex items-center justify-center">9+</span>
                         </Link>
                         <div className="dropdown">
-                            <button tabIndex={0} className="w-9 h-9 rounded-full overflow-hidden border-2 border-cyan-400">
+                            <button tabIndex={0} className="w-7 h- sm:w-9 sm:h-9 rounded-full overflow-hidden border-2 border-cyan-400">
                                 <img
                                     src="https://i.pravatar.cc/150?img=12"
                                     alt="User"
                                     className="w-full h-full object-cover"
                                 />
                             </button>
-                            <div tabIndex="-1" className="dropdown-content z-50 mt-3 bg-cyan-950 rounded-md shadow-lg w-40 py-2 border right-0 border-cyan-700">
+                            <div tabIndex="-1" className="dropdown-content z-50 mt-2 bg-cyan-950 rounded-md shadow-lg w-40 py-2 border right-0 border-cyan-700">
                                 <a href="#" className="block px-4 py-2 hover:bg-gray-700">Profile</a>
                                 <a href="#" className="block px-4 py-2 hover:bg-gray-700">Settings</a>
                                 <a href="#" className="block px-4 py-2 hover:bg-gray-700 text-red-400">Logout</a>
