51f07e66bec490fc8c72f6084723a4ee04e14a7d
abunaim34 <farhanadnanfarabi@gmail.com>
2025-10-29 21:13:04 +0600

    main page content update

--- src/app/page.js
+++ src/app/page.js
@@ -8,11 +8,11 @@ const Home = () => {
         <LeftAside />
       </aside>
       <main className='col-span-12 lg:col-span-6 p-4 border'>
-        <h2 className="font-semibold mb-3">Additional Main Content</h2>
+        <h2 className="font-semibold mb-3">News Feed Upcoming</h2>
         <p className="text-sm text-gray-600">More posts or features can go here.</p>
       </main>
       <aside className='col-span-3 p-4 border'>
-        <h2 className="font-semibold mb-3">Extra Sidebar</h2>
+        <h2 className="font-semibold mb-3">Messenger Upcoming</h2>
         <p className="text-sm text-gray-600">Extra content or widgets here.</p>
       </aside>
     </div>
