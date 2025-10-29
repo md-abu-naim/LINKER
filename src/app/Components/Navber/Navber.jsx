cc7e4e62fbfb368d7faf6430abe126122cdad101
abunaim34 <farhanadnanfarabi@gmail.com>
2025-10-29 20:08:16 +0600

    notification dropdown design

--- src/app/Components/Navber/Navber.jsx
+++ src/app/Components/Navber/Navber.jsx
@@ -36,44 +36,40 @@ const Navber = () => {
                     {/* Right: Message, Notification, Avater Dropdwon */}
                     <div className="flex items-center justify-between gap-2 sm:gap-5">
 
-                        {/* Notification */}
+                        {/* Notification Dropdown */}
                         <div className="dropdown">
                             <button tabIndex={0} title="Notifications" className="relative bg-gray-800 p-1 px-1.5 rounded-full">
                                 <button className="text-xl md:text-2xl hover:text-cyan-400 transition-colors"><FaBell /></button>
                                 <span className="absolute -top-1.5 -right-1.5 bg-red-500 text-white font-semibold rounded-full w-5 h-4 text-[11px] flex items-center justify-center">9+</span>
                             </button>
 
-                            <div tabIndex="-1" className="dropdown-content z-50 mt-2 bg-gray-900 rounded-md shadow-lg w-2xs py-2 border right-0 border-cyan-700">
-                                <Link href="/profile" className="flex items-center gap-2 p-3 mx-3 mb-3">
-                                    <Image src="" alt="User" width={24} height={20} className="inline-block w-9 h-9 border border-cyan-400 rounded-full " />
+                            <div tabIndex="-1" className="dropdown-content z-50 mt-2 bg-gray-900 rounded-md shadow-lg w-lg py-2 border right-0 border-cyan-700">
+                                <h2 className="text-2xl mx-4 font-semibold">Notifications</h2>
+
+                                <Link href="/settings" className="flex items-center gap-2 px-4 py-2 hover:bg-gray-700">
+                                    <Image src="https://i.postimg.cc/65X8XRRf/Face-Care.png" alt="User" width={24} height={20} className="inline-block w-14 h-14 border border-cyan-400 rounded-full " />
                                     <div className="flex flex-col">
-                                        <span className="font-semibold text-sm">Mohammad Abu Naim</span>
+                                        <span className="text-xl">Mohammad abu Naim <span className="text-sm">and 3 others reacted to your photo: {'jfds'}</span></span>
+                                        <small className="text-cyan-400">2m ago</small>
                                     </div>
                                 </Link>
                                 <Link href="/settings" className="flex items-center gap-2 px-4 py-2 hover:bg-gray-700">
-                                    <span className="text-xl bg-gray-800 p-2 rounded-full"><IoSettingsOutline /></span>
-                                    <span className="text-xl">Settings & Privacy</span>
-                                </Link>
-                                <Link href="/help&support" className="flex items-center gap-2 px-4 py-2 hover:bg-gray-700">
-                                    <span className="text-xl bg-gray-800 p-2 rounded-full"><MdHelpOutline /></span>
-                                    <span className="text-xl">Help & Support</span>
-                                </Link>
-                                <Link href="/help&support" className="flex items-center gap-2 px-4 py-2 hover:bg-gray-700">
-                                    <span className="text-xl bg-gray-800 p-2 rounded-full"><MdFeedback /></span>
-                                    <span className="text-xl">Give Feedback</span>
-                                </Link>
-                                <Link href="/help&support" className="flex items-center gap-2 px-4 py-2 hover:bg-gray-700">
-                                    <span className="text-xl bg-gray-800 p-2 rounded-full"><MdLogout /></span>
-                                    <span className="text-xl">Log Out</span>
+                                    <Image src="https://i.postimg.cc/65X8XRRf/Face-Care.png" alt="User" width={24} height={20} className="inline-block w-14 h-14 border border-cyan-400 rounded-full " />
+                                    <div className="flex flex-col">
+                                        <span className="text-xl">Mohammad abu Naim <span className="text-sm">and 3 others mentioned you in a comment</span></span>
+                                        <small className="text-cyan-400">2m ago</small>
+                                    </div>
                                 </Link>
                             </div>
                         </div>
+
+                        {/* Message Dropdown */}
                         <Link href='/message' title="Message" className="relative bg-gray-800 p-1 px-1.5 rounded-full">
                             <button className="text-xl md:text-2xl hover:text-cyan-400 transition-colors"><LuMessageCircleHeart /></button>
                             <span className="absolute -top-1.5 -right-1.5 bg-red-500 text-white font-semibold rounded-full w-5 h-4 text-[11px] flex items-center justify-center">9+</span>
                         </Link>
 
-                        {/* Avater Dropdwon */}
+                        {/* Avater Dropdown */}
                         <div className="dropdown">
                             <button title="Account" tabIndex={0} className="w-8 h-8 sm:w-9 sm:h-9 rounded-full overflow-hidden border-2 border-cyan-400">
                                 <Image src="" alt="User" width={24} height={20} className="w-24 h-6" />
