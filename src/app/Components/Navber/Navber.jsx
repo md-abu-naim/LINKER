55a632ecadc982e33173e7f34be196480b5fef26
abunaim34 <farhanadnanfarabi@gmail.com>
2025-10-29 12:18:55 +0600

    Navber desgin & responsive complete

--- src/app/Components/Navber/Navber.jsx
+++ src/app/Components/Navber/Navber.jsx
@@ -17,35 +17,44 @@ const Navber = () => {
         <div>
             <nav className="w-full bg-gray-900 border-b border-gray-800 shadow-md sticky z-50">
                 <div className="flex justify-between items-center px-3 sm:px-4 py-2">
-                    {/*Left: Logo + Serchbar */}
-                    <div className="flex items-center gap-2 relative">
+
+                    {/*Left: Logo + Searchbar */}
+                    <div className="flex items-center gap-2">
                         <Image src='/Logo.png' alt="Linker" width={38} height={28} className="w-9 h-auto" />
                         <span className="hidden lg:block text-xl font-bold text-cyan-400">LINKER</span>
-                        <ToggleSerchbar />
+                        <div className="hidden md:block">
+                            <ToggleSerchbar />
+                        </div>
                     </div>
 
                     {/*Center: Navlinks */}
-                    <ul className="hidden md:flex items-center font-medium gap-4 sm:gap-6 lg:mr-24">
-                        {navLinks}
-                    </ul>
+                    <div >
+                        <ul className="hidden md:flex items-center font-medium gap-4 sm:gap-6 lg:mr-24">
+                            {navLinks}
+                        </ul>
+                        <div className="md:hidden p-2">
+                            <ToggleSerchbar />
+                        </div>
+                    </div>
 
                     {/* Right: Message, Notification, Avater Dropdwon */}
                     <div className="flex items-center justify-between gap-2 sm:gap-5">
-                        <Link href='/notifications' title="Cart Items" className="relative">
-                            <button className="text-xl md:text-3xl hover:text-cyan-400 transition-colors"><FaBell /></button>
+                        <Link href='/notifications' title="Notifications" className="relative">
+                            <button className="text-2xl hover:text-cyan-400 transition-colors"><FaBell /></button>
                             <span className="absolute -top-1.5 -right-1.5 bg-cyan-500 text-white font-semibold rounded-full w-4 h-4 text-[10px] flex items-center justify-center">9+</span>
                         </Link>
-                        <Link href='/message' title="Cart Items" className="relative z-10">
-                            <button className="text-xl md:text-3xl hover:text-cyan-400 transition-colors"><LuMessageCircleHeart /></button>
+                        <Link href='/message' title="Message" className="relative z-10">
+                            <button className="text-2xl hover:text-cyan-400 transition-colors"><LuMessageCircleHeart /></button>
                             <span className="absolute -top-2 -right-2 bg-cyan-500 text-white text-xs font-semibold rounded-full w-5 h-5 flex items-center justify-center">9+</span>
                         </Link>
                         <div className="dropdown">
                             <button tabIndex={0} className="w-7 h- sm:w-9 sm:h-9 rounded-full overflow-hidden border-2 border-cyan-400">
-                                <img
+                                {/* <img
                                     src="https://i.pravatar.cc/150?img=12"
                                     alt="User"
                                     className="w-full h-full object-cover"
-                                />
+                                /> */}
+                                <Image src="" alt="User" width={24} height={20} className="w-24 h-6" />
                             </button>
                             <div tabIndex="-1" className="dropdown-content z-50 mt-2 bg-cyan-950 rounded-md shadow-lg w-40 py-2 border right-0 border-cyan-700">
                                 <a href="#" className="block px-4 py-2 hover:bg-gray-700">Profile</a>
--- src/app/Components/Navber/ToggleSerchbar.jsx
+++ src/app/Components/Navber/ToggleSerchbar.jsx
@@ -1,53 +1,22 @@
-'use client'
-import { useState } from "react";
-import { FaSearch } from "react-icons/fa";
+
 
 const ToggleSerchbar = () => {
-    const [active, setActive] = useState(false);
     return (
-        <div className="">
-            {/* Large Device Search Bar */}
-            <div className="hidden md:flex items-center bg-gray-800 rounded-full px-3 py-2 ml-3">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5 text-gray-400"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                >
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-4.35-4.35M11 18a7 7 0 100-14 7 7 0 000 14z" />
-                </svg>
-                <input
-                    type="search"
-                    placeholder="Search Linker"
-                    className="bg-transparent outline-none text-sm px-2 w-40 focus:w-44 transition-all duration-300"
-                />
-            </div>
-
-            {/* Small Screen Searchbar */}
-            <div onClick={() => setActive(!active)} className={`${active ? 'hidden' : 'block'} md:hidden bg-gray-800 rounded-full text-gray-400 p-2 w-8 h-8`}>
-                <span><FaSearch></FaSearch></span>
-            </div>
-            <div className={`${active ? 'block absolute top-0 left-0' : 'hidden'} lg:hidden `}>
-                <div className={` flex items-center bg-gray-800 rounded-full px-3 py-2 ml-3`}>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-5 w-5 text-gray-400"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                    >
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-4.35-4.35M11 18a7 7 0 100-14 7 7 0 000 14z" />
-                    </svg>
-                    <input
-                        type="search"
-                        placeholder="Search Linker"
-                        className="bg-transparent z-50 outline-none text-sm px-2 w-fit focus:w-44 transition-all duration-300"
-                    />
-                </div>
-                <div onClick={() => setActive(false)} className="fixed inset-0 bg-transparent z-40"></div>
-            </div>
-
+        <div className="flex items-center bg-gray-800 rounded-full px-3 py-2 flex-1 max-w-xs sm:max-w-sm md:max-w-md">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5 text-gray-400"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+            >
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-4.35-4.35M11 18a7 7 0 100-14 7 7 0 000 14z" />
+            </svg>
+            <input
+                type="search"
+                placeholder="Search Linker"
+                className="bg-transparent outline-none text-sm px-2 w-full"
+            />
         </div>
     );
 };
