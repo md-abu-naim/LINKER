e28acf920370764390084b92f6c2c7a173271601
abunaim34 <farhanadnanfarabi@gmail.com>
2025-10-29 21:21:31 +0600

    home page responsive

--- src/app/Components/HomePage/LeftAside.jsx
+++ src/app/Components/HomePage/LeftAside.jsx
@@ -5,7 +5,7 @@ import { MdNotListedLocation } from "react-icons/md";
 const LeftAside = () => {
     return (
         <div>
-            <Link href={'/profile'} className="shadow-lg rounded-lg border border-gray-800">
+            <Link href={'/profile'} title="Go to Profile" className="shadow-lg rounded-lg border border-gray-800">
                 <Image src="https://i.postimg.cc/RVNGzwQD/Soothing-Green-Tea-Face-Cream.jpg" alt="Cover" width={300} height={250} className="w-full h-28 bg-cover rounded-t-2xl" />
                 <div className="-mt-16 flex flex-col items-center p-3 pb-5">
                     <Image src="https://i.postimg.cc/65X8XRRf/Face-Care.png" alt="" width={100} height={100} className="h-28 w-28 rounded-full border-3 border-cyan-400 object-cover" />
--- src/app/page.js
+++ src/app/page.js
@@ -4,14 +4,14 @@ import LeftAside from './Components/HomePage/LeftAside';
 const Home = () => {
   return (
     <div className='grid grid-cols-12 gap-6 p-4 min-h-screen'>
-      <aside className='col-span-3'>
+      <aside className='col-span-3 hidden lg:block'>
         <LeftAside />
       </aside>
       <main className='col-span-12 lg:col-span-6 p-4 border'>
         <h2 className="font-semibold mb-3">News Feed Upcoming</h2>
         <p className="text-sm text-gray-600">More posts or features can go here.</p>
       </main>
-      <aside className='col-span-3 p-4 border'>
+      <aside className='col-span-3 p-4 hidden lg:block border'>
         <h2 className="font-semibold mb-3">Messenger Upcoming</h2>
         <p className="text-sm text-gray-600">Extra content or widgets here.</p>
       </aside>
