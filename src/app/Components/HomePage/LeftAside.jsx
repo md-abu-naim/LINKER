192f0b5d3641298d33efd551c3fdc186023ebcc7
abunaim34 <farhanadnanfarabi@gmail.com>
2025-10-30 07:38:41 +0600

    left aside design scrolling complete

--- src/app/Components/HomePage/LeftAside.jsx
+++ src/app/Components/HomePage/LeftAside.jsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
+import { FaBookmark, FaUserFriends } from "react-icons/fa";
 import { IoSettingsOutline } from "react-icons/io5";
-import { MdNotListedLocation } from "react-icons/md";
+import { IoMdTimer } from "react-icons/io";
+import { MdNotListedLocation, MdOutlineExplore } from "react-icons/md";
+import { LuMessageCircleHeart } from "react-icons/lu";
 
 const LeftAside = () => {
     return (
@@ -11,7 +14,7 @@ const LeftAside = () => {
                 <div className="-mt-16 flex flex-col items-center p-3 pb-5">
                     <Image src="https://i.postimg.cc/65X8XRRf/Face-Care.png" alt="" width={100} height={100} className="h-28 w-28 rounded-full border-3 border-cyan-400 object-cover" />
                     <h3 className="text-2xl font-bold">Mohammad Abu Naim</h3>
-                    <p className="text-gray-400 text-center text-xm">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quas magnam cum fugit ipsa, eos consequatur quia molestiae excepturi officiis.</p>
+                    <p className="text-gray-400 text-center text-xm">FullStack Developer | Frontend Developer | React | Next.js | Node.js | MERN Stack | MongoDB | Web App</p>
                     <address className="flex items-center gap-2 text-gray-500">
                         <span><MdNotListedLocation /></span>
                         <small>Noakhali, Chittagang, Bangladesh</small>
@@ -19,46 +22,38 @@ const LeftAside = () => {
                 </div>
             </Link>
             <div className="rounded-md shadow-lg py-2 mt-5">
-                <Link href="/settings" className="flex items-center gap-2  py-2 hover:bg-gray-700">
+                <Link href="/friends" className="flex items-center gap-2 p-2 hover:bg-gray-700">
+                    <span className="text-xl bg-gray-800 p-2 rounded-full"><FaUserFriends /></span>
+                    <span className="text-xl">Friends</span>
+                </Link>
+                <Link href="/explore" className="flex items-center gap-2 p-2 hover:bg-gray-700">
+                    <span className="text-xl bg-gray-800 p-2 rounded-full"><MdOutlineExplore /></span>
+                    <span className="text-xl">Explore Videos</span>
+                </Link>
+                <Link href="/saved" className="flex items-center gap-2 p-2 hover:bg-gray-700">
+                    <span className="text-xl bg-gray-800 p-2 rounded-full"><FaBookmark /></span>
+                    <span className="text-xl">Saved Favorites</span>
+                </Link>
+                <Link href="/message" className="flex items-center gap-2 p-2 hover:bg-gray-700">
+                    <span className="text-xl bg-gray-800 p-2 rounded-full"><LuMessageCircleHeart /></span>
+                    <span className="text-xl">Message</span>
+                </Link>
+                <Link href="/memories" className="flex items-center gap-2 p-2 hover:bg-gray-700">
+                    <span className="text-xl bg-gray-800 p-2 rounded-full"><IoMdTimer /></span>
+                    <span className="text-xl">Memories</span>
+                </Link>
+                <Link href="/settings" className="flex items-center gap-2 p-2 hover:bg-gray-700">
                     <span className="text-xl bg-gray-800 p-2 rounded-full"><IoSettingsOutline /></span>
                     <span className="text-xl">Settings & Privacy</span>
                 </Link>
-                <Link href="/settings" className="flex items-center gap-2 px-4 py-2 hover:bg-gray-700">
-                    <span className="text-xl bg-gray-800 p-2 rounded-full"><IoSettingsOutline /></span>
-                    <span className="text-xl">Settings & Privacy</span>
-                </Link>
-                <Link href="/settings" className="flex items-center gap-2 px-4 py-2 hover:bg-gray-700">
-                    <span className="text-xl bg-gray-800 p-2 rounded-full"><IoSettingsOutline /></span>
-                    <span className="text-xl">Settings & Privacy</span>
-                </Link>
-                <Link href="/settings" className="flex items-center gap-2 px-4 py-2 hover:bg-gray-700">
-                    <span className="text-xl bg-gray-800 p-2 rounded-full"><IoSettingsOutline /></span>
-                    <span className="text-xl">Settings & Privacy</span>
-                </Link>
-                <Link href="/settings" className="flex items-center gap-2 px-4 py-2 hover:bg-gray-700">
-                    <span className="text-xl bg-gray-800 p-2 rounded-full"><IoSettingsOutline /></span>
-                    <span className="text-xl">Settings & Privacy</span>
-                </Link>
-                <Link href="/settings" className="flex items-center gap-2 px-4 py-2 hover:bg-gray-700">
-                    <span className="text-xl bg-gray-800 p-2 rounded-full"><IoSettingsOutline /></span>
-                    <span className="text-xl">Settings & Privacy</span>
-                </Link>
-                <Link href="/settings" className="flex items-center gap-2 px-4 py-2 hover:bg-gray-700">
-                    <span className="text-xl bg-gray-800 p-2 rounded-full"><IoSettingsOutline /></span>
-                    <span className="text-xl">Settings & Privacy</span>
-                </Link>
-                <Link href="/help&support" className="flex items-center gap-2 px-4 py-2 hover:bg-gray-700">
+                <Link href="/help&support" className="flex items-center gap-2 p-2 hover:bg-gray-700">
                     <span className="text-xl bg-gray-800 p-2 rounded-full"><IoSettingsOutline /></span>
                     <span className="text-xl">Help & Support</span>
                 </Link>
-                <Link href="/feedback" className="flex items-center gap-2 px-4 py-2 hover:bg-gray-700">
+                <Link href="/feedback" className="flex items-center gap-2 p-2 hover:bg-gray-700">
                     <span className="text-xl bg-gray-800 p-2 rounded-full"><IoSettingsOutline /></span>
                     <span className="text-xl">Give Feedback</span>
                 </Link>
-                <button className="flex items-center gap-2 px-4 py-2 hover:bg-gray-700">
-                    <span className="text-xl bg-gray-800 p-2 rounded-full"><IoSettingsOutline /></span>
-                    <span className="text-xl">Log Out</span>
-                </button>
             </div>
         </>
     );
