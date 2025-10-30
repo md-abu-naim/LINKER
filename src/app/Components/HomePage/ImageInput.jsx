6f2ee72d788ace71f32edb7e3ce9e45d8ae69418
abunaim34 <farhanadnanfarabi@gmail.com>
2025-10-30 21:27:11 +0600

    fileficker

--- src/app/Components/HomePage/ImageInput.jsx
+++ src/app/Components/HomePage/ImageInput.jsx
@@ -3,14 +3,11 @@ import { FaPhotoVideo } from "react-icons/fa";
 
 const ImageInput = () => {
 
-    const hanldeClick = async() => {
-        console.log('object');
-    }
     return (
-        <button onClick={hanldeClick} className="flex items-center gap-2">
+        <button onClick={() => document.getElementById('fileInput').click()} className="flex items-center gap-2">
             <span className="text-2xl text-green-500"><FaPhotoVideo /></span>
             <span className="text-2xl">Photo/Video</span>
-            <input type="file" accept="image/*,video/*" className="hidden" />
+            <input id="fileInput" type="file" className="hidden" />
         </button>
     );
 };
