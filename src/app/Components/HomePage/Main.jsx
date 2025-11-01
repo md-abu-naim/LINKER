775f082212fe48a0ecb6f199eac4b06a02e36c75
abunaim34 <farhanadnanfarabi@gmail.com>
2025-11-01 06:39:34 +0600

    select options

--- src/app/Components/HomePage/Main.jsx
+++ src/app/Components/HomePage/Main.jsx
@@ -30,7 +30,7 @@ const Main = () => {
                 </div>
 
                 {/* Post Box Modal */}
-                <input type="checkbox" id="my_modal_6" className="modal-toggle" />
+                <input type="checkbox" id="my_modal_6" className="modal-toggle h-full" />
                 <div className="modal" role="dialog">
                     <div className="modal-box relative">
                         <div className="flex items-center justify-center pb-4">
@@ -38,27 +38,28 @@ const Main = () => {
                             <label htmlFor="my_modal_6" className="absolute right-6 bg-gray-700 hover:bg-gray-800 rounded-full p-2 text-xl"><RxCross1 /></label>
                         </div>
                         <hr className="text-gray-700" />
+
+                        {/* Profile */}
                         <div className="flex items-center gap-2 mt-3">
                             <Image src="https://i.postimg.cc/65X8XRRf/Face-Care.png" alt="User" width={24} height={20} className="inline-block w-11 h-11 border border-cyan-400 rounded-full " />
                             <div className="flex flex-col">
                                 <span className="font-semibold text-md">Mohammad Abu Naim</span>
 
-                                <select defaultValue="Pick a font" className="select select-ghost bg-gray-700">
-                                    <option>
-                                        <span><MdPublic /></span>
-                                        <span>Public</span>
-                                    </option>
-                                    <option>
-                                        <span><MdPublic /></span>
-                                        <span>Friends</span>
-                                    </option>
-                                    <option>
-                                        <span><MdPublic /></span>
-                                        <span>Only Me</span>
-                                    </option>
+                                <select name="visibility" defaultValue="public" className="select w-28 h-4 bg-gray-700 border-none outline-none">
+                                    <option value="public">🌍 Public</option>
+                                    <option value="friends">👥 Friends</option>
+                                    <option value="private">🔒 Only Me</option>
                                 </select>
                             </div>
                         </div>
+
+                        {/* Textarea */}
+                        {/* <textarea className="w-full focus:outline-none resize-none rounded" placeholder="Whats on your mind? Mohammad Abu?" rows={5}></textarea> */}
+                        <textarea
+                            className="w-full  p-4 text-lg focus:outline-none resize-none rounded"
+                            placeholder="Whats on your mind? Mohammad Abu?"
+                            rows={5}
+                        />
                     </div>
                 </div>
             </div>
