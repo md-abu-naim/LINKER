1f2d3a301aa60c650e373d02bcf4c562a0d25f24
abunaim34 <farhanadnanfarabi@gmail.com>
2025-11-03 21:29:46 +0600

    post image grid layout overlay that more then 4 img

--- src/app/Components/HomePage/PostImage.jsx
+++ src/app/Components/HomePage/PostImage.jsx
@@ -12,7 +12,7 @@ const PostImage = () => {
         'https://i.postimg.cc/RVNGzwQD/Soothing-Green-Tea-Face-Cream.jpg',
         'https://i.postimg.cc/RVNGzwQD/Soothing-Green-Tea-Face-Cream.jpg',
         'https://i.postimg.cc/RVNGzwQD/Soothing-Green-Tea-Face-Cream.jpg',
-        // 'https://i.postimg.cc/RVNGzwQD/Soothing-Green-Tea-Face-Cream.jpg',
+        'https://i.postimg.cc/RVNGzwQD/Soothing-Green-Tea-Face-Cream.jpg',
     ]
 
     const imageCount = images?.length - 4
@@ -31,7 +31,7 @@ const PostImage = () => {
                 <Image className='w-full h-64 rounded-xl bg-cover' src={images[0]} width={600} height={700} alt='Images' />
                 <div className='grid grid-cols-2 gap-2'>
                     {
-                        images.slice(1,3).map(img => (
+                        images.slice(1, 3).map(img => (
                             <Image key={img} className='rounded-xl bg-cover' src={img} width={600} height={700} alt='Images' />
                         ))
                     }
@@ -43,12 +43,16 @@ const PostImage = () => {
         <div>
             <div className='w-full mt-2 grid grid-cols-2 gap-2'>
                 {
-                    images.slice(0,4).map(img => (
-                        <div key={img} className='relative'>
-                            <Image key={img} className='rounded-xl bg-cover' src={img} width={600} height={700} alt='Images' />
-                            <div className='absolute inset-0 bg-black/'>
-                                <span>{imageCount}</span>
-                            </div>
+                    images.slice(0, 4).map((img, index) => (
+                        <div key={index} className='relative'>
+                            <Image className='rounded-xl bg-cover' src={img} width={600} height={700} alt={`Post Image ${index + 1}`} />
+                            {
+                                index === 3 && imageCount > 0 && (
+                                    <div className='absolute inset-0 bg-black/60 flex items-center justify-center rounded-xl'>
+                                        <span className='text-white text-2xl font-semibold'>+{imageCount}</span>
+                                    </div>
+                                )
+                            }
                         </div>
                     ))
                 }
@@ -76,25 +80,25 @@ const PostImage = () => {
 
 export default PostImage;
 //   return (
-//     <div className="w-full mt-2 grid grid-cols-2 gap-2">
-//       {images.slice(0, 4).map((img, index) => (
-//         <div key={index} className="relative">
-//           <Image
-//             src={img}
-//             alt={`Post Image ${index + 1}`}
-//             width={400}
-//             height={300}
-//             className="w-full h-48 rounded-xl object-cover"
-//           />
-//           {/* + more overlay */}
-//           {index === 3 && extraCount > 0 && (
-//             <div className="absolute inset-0 bg-black/60 flex items-center justify-center rounded-xl">
-//               <span className="text-white text-2xl font-semibold">+{extraCount}</span>
-//             </div>
-//           )}
+// <div className="w-full mt-2 grid grid-cols-2 gap-2">
+//   {images.slice(0, 4).map((img, index) => (
+//     <div key={index} className="relative">
+//       <Image
+//         src={img}
+//         alt={`Post Image ${index + 1}`}
+//         width={400}
+//         height={300}
+//         className="w-full h-48 rounded-xl object-cover"
+//       />
+//       {/* + more overlay */}
+//       {index === 3 && extraCount > 0 && (
+//         <div className="absolute inset-0 bg-black/60 flex items-center justify-center rounded-xl">
+//           <span className="text-white text-2xl font-semibold">+{extraCount}</span>
 //         </div>
-//       ))}
+//       )}
 //     </div>
+//   ))}
+// </div>
 //   );
 // };
 
