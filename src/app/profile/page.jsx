97c26984309016cf1bc9c071577c0e07cf1e2f71
abunaim34 <farhanadnanfarabi@gmail.com>
2025-11-15 12:37:14 +0600

    profile page > message followers & post button

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -36,30 +36,35 @@ const Profile = () => {
                             <span className="text-2xl font-medium">(Naim)</span>
                         </div>
                     </div>
-                    <button className="p-2 rounded-full hover:bg-gray-700 transition">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-6 w-6 text-white"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 12h.01M12 12h.01M18 12h.01" />
-                        </svg>
-                    </button>
+                    <div className="flex items-center gap-2">
+                        <button className="flex items-center bg-gray-800 hover:bg-gray-900 text-cyan-400 hover:text-cyan-300 rounded-md p-2">
+                            <span className="text-md">Follow</span>
+                        </button>
+                        <button className="bg-gray-800 hover:bg-gray-900 text-cyan-400 hover:text-cyan-300 rounded-md p-2">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-6 w-6"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 12h.01M12 12h.01M18 12h.01" />
+                            </svg>
+                        </button>
+                    </div>
                 </div>
                 <div className="flex items-center justify-around mt-10 bg-gray-900">
-                    <button className="flex flex-col items-center hover:bg-gray-800 p-5">
+                    <button className="flex flex-col items-center hover:bg-gray-800 hover:text-cyan-400 p-5">
                         <span className="text-2xl"><LuMessageCircleHeart /></span>
                         <span className="text-md">Message</span>
                     </button>
-                    <button className="flex items-center gap-0.5 text-lg bg-gray-800 hover:bg-gray-900 p-2 rounded-md hover:text-cyan-400 transition">
-                        <span><FaUserFriends /></span>
-                        <span>Message</span>
+                    <button className="flex flex-col items-center hover:bg-gray-800 hover:text-cyan-400 p-5">
+                        <span className="text-2xl">1800</span>
+                        <span className="text-md">Followers</span>
                     </button>
-                    <button className="flex items-center gap-0.5 text-lg bg-gray-800 hover:bg-gray-900 p-2 rounded-md hover:text-cyan-400 transition">
-                        <span><FaUserFriends /></span>
-                        <span>Message</span>
+                    <button className="flex flex-col items-center hover:bg-gray-800 hover:text-cyan-400 p-5">
+                        <span className="text-2xl">122</span>
+                        <span className="text-md">Total Posts</span>
                     </button>
                 </div>
             </div>
