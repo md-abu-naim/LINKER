2452f952ad2ba9feda3e0d99f4d35a1aefa66f57
abunaim34 <farhanadnanfarabi@gmail.com>
2025-11-15 19:36:53 +0600

    profile page > post box

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -3,18 +3,16 @@ import { LuMessageCircleHeart } from "react-icons/lu";
 import { FiBookmark, FiCalendar, FiEdit2, FiHome, FiImage, FiLink, FiShare2, FiSlash, FiUser, FiUserPlus, FiUsers, FiUserX } from "react-icons/fi";
 import { IoMdTimer } from "react-icons/io";
 import Link from "next/link";
+import { FaVideo } from "react-icons/fa";
+import { MediaInput } from "../Components/HomePage/MediaInput";
+import { BsEmojiSunglasses } from "react-icons/bs";
+import PostModal from "../Components/HomePage/PostModal";
 
 const Profile = () => {
-    const posts = [
-        { id: 1, img: "https://i.postimg.cc/25BPjPQg/Aloe-Neem-Anti-Dandruff-Shampoo.webp" },
-        { id: 2, img: "https://i.postimg.cc/h4M3nDFf/Rose-Petal-Hydrating-Face-Mist.jpg" },
-        { id: 3, img: "https://i.postimg.cc/0NmB17xs/Herbal-Strengthening-Hair-Oil.webp" },
-        { id: 4, img: "https://i.postimg.cc/WbbCqDmG/Cocoa-Butter-Body-Lotion.jpg" },
-        { id: 5, img: "https://i.postimg.cc/x1B4Ztw5/Exfoliating-Coffee-Body-Scrub.webp" },
-    ];
     return (
         <div>
             <div className="">
+                {/* Full Profile Information */}
                 <div className="bg-gradient-to-b from-gray-900 via-gray-850 to-gray-950 shadow-[0_0_35px_rgba(0,0,0,0.5)]">
                     {/* Cover */}
                     <div className="flex items-center justify-center shadow-gray-300">
@@ -143,6 +141,49 @@ const Profile = () => {
                     </div>
                 </div>
 
+                {/* Aside & Posts */}
+                <div className="grid grid-cols-12 gap-6 max-w-5xl mx-auto mt-2">
+
+                    <div className="col-span-5 border">aside</div>
+
+                    <div className="col-span-7">
+                        <div className="rounded-3xl p-4 mt-4 md:mt-0 md:p-6 bg-gradient-to-b from-gray-900 via-gray-850 to-gray-950 backdrop-blur-xl border border-gray-800 shadow-[0_0_30px_rgba(0,0,0,0.4)] hover:shadow-[0_0_50px_rgba(0,255,200,0.15)] transition-all duration-500 hover:-translate-y-1">
+                            {/* Header */}
+                            <div className="flex items-center gap-3 mb-3 md:mb-4">
+                                <Link href='/profile'>
+                                    <Image src="https://i.postimg.cc/65X8XRRf/Face-Care.png" alt="User" width={50} height={50} className="w-9 h-9 md:w-12 md:h-12 border border-cyan-400 rounded-full hover:scale-105 transition-all duration-300" />
+                                </Link>
+                                <div className="flex flex-col">
+                                    <h3 className="text-sm md:text-lg font-semibold text-gray-100">Mohammad Abu Naim</h3>
+                                    <p className="text-xs md:text-sm text-gray-500">Share something inspiring ✨</p>
+                                </div>
+                            </div>
+
+                            {/* Input field */}
+                            <label htmlFor="my_modal_6" className="block w-full rounded-2xl bg-gray-800/60 text-gray-300 md:min-h-[90px] px-2 md:px-5 py-2 md:py-4  cursor-pointer hover:bg-gray-700/70 transition-all duration-300 border border-transparent hover:border-cyan-500">
+                                <span className="opacity-70">What do you want to share today?</span>
+                            </label>
+
+                            {/* Buttons */}
+                            <div className="hidden md:flex mt-4 items-center justify-around border-t border-gray-700 pt-4">
+                                <button className="flex items-center gap-2 bg-gray-800/70 px-3 py-2 rounded-xl hover:bg-gray-700/80 transition-all duration-300 group">
+                                    <FaVideo className="text-red-500 text-xl group-hover:scale-110 transition-transform" />
+                                    <span className="text-gray-300 font-medium">Go Live</span>
+                                </button>
+
+                                <MediaInput />
+
+                                <button className="flex items-center gap-2 bg-gray-800/70 px-3 py-2 rounded-xl hover:bg-gray-700/80 transition-all duration-300 group">
+                                    <BsEmojiSunglasses className="text-yellow-500 text-xl group-hover:scale-110 transition-transform" />
+                                    <span className="text-gray-300 font-medium">Feelings</span>
+                                </button>
+                            </div>
+                        </div>
+
+                        <input type="checkbox" id="my_modal_6" className="modal-toggle" />
+                        <PostModal />
+                    </div>
+                </div>
             </div>
 
         </div>
