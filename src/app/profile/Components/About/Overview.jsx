2fcb133a3f363ec7df9f9c6a4e6df3b7cdfc6e38
abunaim34 <farhanadnanfarabi@gmail.com>
2025-11-16 21:22:47 +0600

    profile page ==> about > overview section fully complete

--- src/app/profile/Components/About/Overview.jsx
+++ src/app/profile/Components/About/Overview.jsx
@@ -5,7 +5,7 @@ import { MdMarkEmailUnread } from "react-icons/md";
 
 const Overview = () => {
     return (
-        <div className="bg-gradient-to-b from-gray-950 via-gray-950 to-gray-900 border border-gray-800 rounded-2xl backdrop-blur-xl p-4 shadow-[0_0_35px_rgba(0,0,0,0.3)] hover:-translate-y-1">
+        <div className="bg-gradient-to-b from-gray-950 via-gray-950 to-gray-900 border border-gray-800 rounded-2xl backdrop-blur-xl p-4 mb-16 lg:mb-3 shadow-[0_0_35px_rgba(0,0,0,0.3)] hover:-translate-y-1">
             {/* Works */}
             <div className="flex items-center gap-3 text-md hover:bg-gray-800 p-2 rounded-sm">
                 <FaBriefcase className="text-3xl text-cyan-400" />
