4c6c4140c8b2199fefcf91d85d9666a36aeae6c3
abunaim34 <farhanadnanfarabi@gmail.com>
2025-11-18 21:13:39 +0600

    profile page ==> about > work&education section complete

--- src/app/profile/Components/About/AddSchoolForm.jsx
+++ src/app/profile/Components/About/AddSchoolForm.jsx
@@ -1,6 +1,13 @@
+'use client'
 import { FiPlus } from "react-icons/fi";
 
 const AddSchoolForm = () => {
+
+    const handleSchool = e => {
+        e.preventDefault()
+        const school = e.target.school.value
+        console.log(school);
+    }
     return (
         <div>
             <input type="checkbox" id="toggleSchoolForm" className="peer hidden" />
@@ -11,10 +18,10 @@ const AddSchoolForm = () => {
                     <span className="text-md">Add School</span>
                 </label>
             </div>
-            <form className="space-y-5 hidden peer-checked:block bg-gray-950/40 p-6 rounded-xl border border-gray-800 shadow-[0_0_20px_rgba(0,255,255,0.04)] mt-2">
+            <form onSubmit={handleSchool} className="space-y-5 hidden peer-checked:block bg-gray-950/40 p-6 rounded-xl border border-gray-800 shadow-[0_0_20px_rgba(0,255,255,0.04)] mt-2">
                 <div className="flex flex-col gap-1">
                     <label className="text-sm text-cyan-400 font-medium">School</label>
-                    <input type="text" name="company" className="p-3 rounded-lg bg-gray-900 border border-gray-700 focus:border-cyan-400 text-gray-200 transition-all outline-none shadow-[inset_0_0_10px_rgba(0,0,0,0.5)]" placeholder="Ex: Islamia School" required />
+                    <input type="text" name="school" className="p-3 rounded-lg bg-gray-900 border border-gray-700 focus:border-cyan-400 text-gray-200 transition-all outline-none shadow-[inset_0_0_10px_rgba(0,0,0,0.5)]" placeholder="Ex: Islamia School" required />
                 </div>
                 <button className="w-full bg-gradient-to-r from-cyan-500 to-blue-600 hover:from-cyan-400 hover:to-blue-500 py-3 rounded-2xl font-semibold text-lg transition-all duration-300 ">
                     Share School 🚀
