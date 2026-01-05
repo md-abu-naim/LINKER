38e7692ff69a032d9064ed155b5a5284fff79388
abunaim34 <farhanadnanfarabi@gmail.com>
2026-01-05 08:36:00 +0600

    register page form

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -18,18 +18,14 @@ const Register = () => {
                         </div>
                         <div className="flex flex-col gap-1 w-full">
                             <label className="text-sm text-cyan-400 font-medium">Last Name</label>
-                            <input type="text" name="lastName" className="p-3 rounded-lg bg-gray-900 border border-gray-700 focus:border-cyan-400 text-gray-200 transition-all outline-none shadow-[inset_0_0_10px_rgba(0,0,0,0.5)]" placeholder="Ex: RasselF" />
+                            <input type="text" name="lastName" className="p-3 rounded-lg bg-gray-900 border border-gray-700 focus:border-cyan-400 text-gray-200 transition-all outline-none shadow-[inset_0_0_10px_rgba(0,0,0,0.5)]" placeholder="Ex: Rassel" />
                         </div>
                     </div>
 
                     <button className="w-full bg-gradient-to-r from-cyan-500 to-blue-600 hover:from-cyan-400 hover:to-blue-500 py-3 rounded-2xl font-semibold text-lg transition-all duration-300 ">
-                        Log in
+                        Register
                     </button>
                 </form>
-
-                <p className='text-md font-medium text-center text-cyan-500 underline cursor-pointer'>Forggotten Password</p>
-
-                <div className="divider">OR</div>
             </div>
         </div>
     );
