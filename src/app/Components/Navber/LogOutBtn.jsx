74f8546d5b786da91f7d8ec510d0c5508bcdf784
abunaim34 <farhanadnanfarabi@gmail.com>
2026-02-19 22:26:15 +0600

    logOut with redirect

--- src/app/Components/Navber/LogOutBtn.jsx
+++ src/app/Components/Navber/LogOutBtn.jsx
@@ -4,7 +4,7 @@ import { MdLogout } from "react-icons/md";
 
 const LogOutBtn = () => {
     return (
-        <button onClick={() => signOut()} className="flex items-center gap-2 px-4 py-2 hover:bg-gray-700 w-full">
+        <button onClick={() => signOut({callbackUrl: '/login'})} className="flex items-center gap-2 px-4 py-2 hover:bg-gray-700 w-full">
             <span className="text-xl bg-gray-800 p-2 rounded-full"><MdLogout /></span>
             <span className="text-xl">Log Out</span>
         </button>
