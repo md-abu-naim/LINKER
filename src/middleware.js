6f59c775b2dc4a333ad6a6e693a6c8854c03659d
abunaim34 <farhanadnanfarabi@gmail.com>
2026-03-07 10:49:21 +0600

    middleware setup

--- src/middleware.js
+++ src/middleware.js
@@ -1,3 +1,26 @@
-export const middleware =(req) => {
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
 
+
+export const middleware = async (req) => {
+    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
+    
+    const { pathname } = req.nextUrl
+    
+    const publicRoutes = ["/login", "/register", "/edit-profile"]
+    
+    if (!token && !publicRoutes.includes(pathname)) {
+        return NextResponse.redirect(new URL('/login', req.url))
+    }
+
+    if (token && publicRoutes.includes(pathname)) {
+        return NextResponse.redirect(new URL('/', req.url))
+    }
+    
+    return NextResponse.next()
+}
+
+
+export const config = {
+    matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"]
 }
