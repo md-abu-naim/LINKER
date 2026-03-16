06325b97dd4970e78fcb403f7406cfe7f33d6c38
abunaim34 <farhanadnanfarabi@gmail.com>
2026-03-16 10:03:56 +0600

    update more then three photo or vidoe showing

--- src/app/Components/Shared/ImageLayout.jsx
+++ src/app/Components/Shared/ImageLayout.jsx
@@ -6,7 +6,6 @@ import { useState } from 'react';
 const ImageLayout = ({ media, post }) => {
     const [openModal, setOpenModal] = useState(false)
     const imageCount = media?.length - 4
-    console.log(media);
 
 
     // Image Layout
@@ -15,8 +14,8 @@ const ImageLayout = ({ media, post }) => {
             return (
                 <div onClick={() => setOpenModal(true)} className='mt-2'>
                     {
-                        media[0].type === 'image' ? <Image className='w-full h-auto rounded-xl bg-cover cursor-pointer' src={media[0].url} width={600} height={700} alt='Images' /> :
-                            <video src={media[0].url} controls className='w-full h-auto rounded-xl bg-cover cursor-pointer' />
+                        media[0].type === 'image' ? <Image className='w-full h-auto rounded-xl object-cover cursor-pointer' src={media[0].url} width={600} height={700} alt='Images' /> :
+                            <video src={media[0].url} controls className='w-full h-auto rounded-xl object-cover cursor-pointer' />
                     }
                 </div>
             )
@@ -25,12 +24,13 @@ const ImageLayout = ({ media, post }) => {
         if (media?.length === 3) {
             return (
                 <div onClick={() => setOpenModal(true)} className='w-full mt-2 space-y-1 cursor-pointer'>
-                    {media[0].type === 'image' ? <Image className='w-full h-36 md:h-64 rounded-sm bg-cover' src={media[0].url} width={600} height={700} alt='Images' /> : <video controls className='w-full h-36 md:h-64 rounded-sm bg-cover' src={media[0].url} width={600} height={700} />}
+                    {media[0].type === 'image' ? <Image className='w-full h-36 md:h-64 rounded-sm object-cover' src={media[0].url} width={600} height={700} alt='Images' /> :
+                        <video controls className='w-full h-36 md:h-64 rounded-sm object-cover' src={media[0].url} width={600} height={700} />}
                     <div className='grid grid-cols-2 gap-1'>
                         {
                             media?.slice(1, 3).map((item, i) => (
-                                item.type === 'image' ? <Image key={i} className='h-36 md:h-64 w-full rounded-sm bg-cover' src={item.url} width={600} height={700} alt='Images' /> :
-                                <video key={i} controls className='h-36 md:h-64 w-full rounded-sm bg-cover' src={item.url} width={600} height={700} />
+                                item.type === 'image' ? <Image key={i} className='h-36 md:h-64 w-full rounded-sm object-cover' src={item.url} width={600} height={700} alt='Images' /> :
+                                    <video key={i} controls className='h-36 md:h-64 w-full rounded-sm object-cover' src={item.url} width={600} height={700} />
                             ))
                         }
                     </div>
@@ -38,24 +38,27 @@ const ImageLayout = ({ media, post }) => {
             )
         }
 
-        // return (
-        //     <div onClick={() => setOpenModal(true)} className='w-full mt-2 grid grid-cols-2 gap-1'>
-        //         {
-        //             media?.slice(0, 4).map((img, index) => (
-        //                 <div key={index} className='relative cursor-pointer'>
-        //                     <Image className='h-36 md:h-64 w-full rounded-sm bg-cover' src={img} width={600} height={700} alt={`Post Image ${index + 1}`} />
-        //                     {
-        //                         index === 3 && imageCount > 0 && (
-        //                             <div className='absolute inset-0 bg-black/60 flex items-center justify-center rounded-sm'>
-        //                                 <span className='text-white text-2xl font-semibold'>+{imageCount}</span>
-        //                             </div>
-        //                         )
-        //                     }
-        //                 </div>
-        //             ))
-        //         }
-        //     </div>
-        // )
+        return (
+            <div onClick={() => setOpenModal(true)} className='w-full mt-2 grid grid-cols-2 gap-1'>
+                {
+                    media?.slice(0, 4).map((item, index) => (
+                        <div key={index} className='relative cursor-pointer'>
+                            {
+                                item.type === 'image' ? <Image className='h-36 md:h-64 w-full rounded-sm object-cover' src={item.url} width={600} height={700} alt={`Post Image ${index + 1}`} /> :
+                                    <video className='h-36 md:h-64 w-full rounded-sm object-cover' src={item.url} width={600} height={700} alt={`Post Image ${index + 1}`} />
+                            }
+                            {
+                                index === 3 && imageCount > 0 && (
+                                    <div className='absolute inset-0 bg-black/60 flex items-center justify-center rounded-sm'>
+                                        <span className='text-white text-2xl font-semibold'>+{imageCount}</span>
+                                    </div>
+                                )
+                            }
+                        </div>
+                    ))
+                }
+            </div>
+        )
     }
 
     return (
