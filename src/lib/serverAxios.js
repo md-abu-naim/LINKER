efa94cf66ff8b46c897ae405479a6a24821b9e00
abunaim34 <farhanadnanfarabi@gmail.com>
2026-03-16 14:05:38 +0600

    update serverAxios env

--- src/lib/serverAxios.js
+++ src/lib/serverAxios.js
@@ -3,24 +3,6 @@ import { getServerSession } from "next-auth"
 import axios from "axios"
 
 
-// export const serverAxios = async(url, options = {}) => {
-//     const session = await getServerSession(authOptions)
-//     console.log(session);
-
-//     const res = await axios({
-//         url: `${process.env.NEXT_PUBLIC_API}${url}`,
-//         method: options.method || 'GET',
-//         data: options.data || {},
-//         headers: {
-//             Authorization: `Bearer ${session?.accessToken}`
-//         }
-//     })
-//     return res.data.data
-// }
-
-// import { authOptions } from "@/app/api/auth/[...nextauth]/route"
-// import { getServerSession } from "next-auth"
-// import axios from "axios"
 
 export const serverAxios = async (url, options = {}) => {
   try {
