1bafe897b914bb34fe5c6c3ea4de768639997aae
abunaim34 <farhanadnanfarabi@gmail.com>
2026-03-16 13:28:28 +0600

    some update

--- src/lib/serverAxios.js
+++ src/lib/serverAxios.js
@@ -1,18 +1,47 @@
+// import { authOptions } from "@/app/api/auth/[...nextauth]/route"
+// import { getServerSession } from "next-auth"
+// import axios from "axios"
+
+
+// export const serverAxios = async(url, options = {}) => {
+//     const session = await getServerSession(authOptions)
+
+//     const res = await axios({
+//         url: `${process.env.NEXT_PUBLIC_API}${url}`,
+//         method: options.method || 'GET',
+//         data: options.data || {},
+//         headers: {
+//             Authorization: `Bearer ${session?.accessToken}`
+//         }
+//     })
+//     return res.data.data
+// }
+
 import { authOptions } from "@/app/api/auth/[...nextauth]/route"
 import { getServerSession } from "next-auth"
 import axios from "axios"
 
-
-export const serverAxios = async(url, options = {}) => {
+export const serverAxios = async (url, options = {}) => {
+  try {
     const session = await getServerSession(authOptions)
 
+    if (!session?.accessToken) {
+      console.log("No access token")
+      return null
+    }
+
     const res = await axios({
-        url: `${process.env.NEXT_PUBLIC_API}${url}`,
-        method: options.method || 'GET',
-        data: options.data || {},
-        headers: {
-            Authorization: `Bearer ${session?.accessToken}`
-        }
+      url: `${process.env.NEXT_PUBLIC_API}${url}`,
+      method: options.method || "GET",
+      data: options.data || {},
+      headers: {
+        Authorization: `Bearer ${session.accessToken}`,
+      },
     })
+
     return res.data.data
+  } catch (error) {
+    console.error("SERVER AXIOS ERROR:", error?.response?.status)
+    return null
+  }
 }
