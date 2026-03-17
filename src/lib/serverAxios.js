08416f23039a83ff0e7fdbd04e188221d910f0f8
abunaim34 <farhanadnanfarabi@gmail.com>
2026-03-17 09:31:27 +0600

    update serverAxios .env

--- src/lib/serverAxios.js
+++ src/lib/serverAxios.js
@@ -3,11 +3,28 @@ import { getServerSession } from "next-auth"
 import axios from "axios"
 
 
+// export const serverAxios = async(url, options = {}) => {
+//     const session = await getServerSession(authOptions)
+//     console.log(session);
+
+//     const res = await axios({
+//         url: `${process.env.NEXT_PUBLIC_API}${url}`,
+//         method: options.method || 'GET',
+//         data: options.data || {},
+//         headers: {
+//             Authorization: `Bearer ${session?.accessToken}`
+//         }
+//     })
+//     return res.data.data
+// }
+
+// import { authOptions } from "@/app/api/auth/[...nextauth]/route"
+// import { getServerSession } from "next-auth"
+// import axios from "axios"
 
 export const serverAxios = async (url, options = {}) => {
   try {
     const session = await getServerSession(authOptions)
-    console.log(session);
 
     if (!session?.accessToken) {
       console.log("No access token")
